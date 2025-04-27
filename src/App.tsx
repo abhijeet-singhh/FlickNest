b4c1091a493bd075670d1d64926889abe1ca2448
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-04-27 15:13:47 +0530

    feat: add routing and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Search from "./pages/Search"
+import Explore from "./pages/Explore"
+import Details from "./pages/Details"
 
 const App = () => {
   return (
     <div className="bg-black text-white h-screen">
       <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/explore/:id" element={<Details />} />
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,30 @@
 import { CiSearch } from "react-icons/ci"
 import { FaUserAlt } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const menuClass: string = "text-neutral-400 hover:text-lime-500 cursor-pointer transition-colors duration-300"
 
 const Navbar = () => {
   return (
-    <nav className="bg-neutral-900 w-full h-16 flex items-center justify-between px-10">
-        <div className="flex items-center gap-20">
-            <span className="text-lime-600 text-2xl font-bold cursor-pointer">CINEMAX</span>
+    <nav className="bg-neutral-900 w-full h-16 flex items-center justify-between px-2 md:px-10">
+      <div className="flex items-center gap-20">
+        <Link to="/" className="text-lime-600 text-2xl font-bold cursor-pointer">CINEMAX</Link>
 
-            <div className="flex items-center gap-8 ">
-                <span className={menuClass}>TV Shows</span>
-                <span className={menuClass}>Movies</span>
-                <span className={menuClass}>Popular</span>
-            </div>
+        <div className="hidden lg:flex items-center gap-8 ">
+          <Link to="/explore" className={menuClass}>TV Shows</Link>
+          <Link to="/explore" className={menuClass}>Movies</Link>
+          <Link to="/explore" className={menuClass}>Popular</Link>
         </div>
-        <div className="flex items-center gap-8">
-            <div className="flex items-center gap-2">
-            <input className="outline-none w-44 px-2 text-neutral-400" type="text" placeholder="Search Movies..." />
-            <CiSearch size={24} className="cursor-pointer" />
-            </div>
-            <FaUserAlt className="text-gray-300 cursor-pointer" />
+      </div>
+      <div className="flex items-center gap-4 md:gap-8">
+        <div className="flex items-center gap-2">
+          <form>
+            <input className="outline-none w-44 px-2 text-neutral-400 bg-transparent" type="text" placeholder="Search Movies..." />
+          </form>
+          <CiSearch size={24} className="cursor-pointer" />
         </div>
+        <FaUserAlt className="text-gray-300 cursor-pointer" />
+      </div>
     </nav>
   )
 }
