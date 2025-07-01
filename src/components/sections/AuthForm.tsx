f4f130bc5686bf549b19390db7cc034bb9a7d5bf
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2025-07-02 01:44:08 +0530

    feat(auth): add email login selection screen and conditional form rendering

--- src/components/sections/AuthForm.tsx
+++ src/components/sections/AuthForm.tsx
@@ -8,14 +8,14 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "../../store/slices/authSlice";
 
-// ✅ Styles
+// Styles
 const inputDivClass =
   "group px-3 flex items-center justify-center gap-4 text-gray-300 rounded-xl border-2 border-gray-700 focus-within:border-[#b1d960] transition-all duration-500";
 
 const inputIconClass =
   "text-gray-400 group-focus-within:text-[#b1d960] transition-all duration-500";
 
-// ✅ Validation Schemas
+// Validation Schemas
 const baseSchema = z.object({
   email: z.string().email("Invalid email"),
   password: z.string().min(6, "Password must be at least 6 characters"),
@@ -35,8 +35,10 @@ const AuthForm = () => {
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [authMethod, setAuthMethod] = useState<"none" | "email">("none");
 
-  // ✅ Dynamic Schema based on isLogin state
+
+  // Dynamic Schema based on isLogin state
   const schema = useMemo(() => (isLogin ? loginSchema : registerSchema), [isLogin]); //useMemo just helps optimize performance — it avoids recalculating the schema unless isLogin changes.
 
   const {
@@ -55,7 +57,7 @@ const AuthForm = () => {
   };
 
   return (
-    <div className="relative h-dvh w-full flex items-center justify-center overflow-hidden">
+    <div className="relative h-dvh w-full flex items-center justify-center overflow-hidden bg-gradient-to-bl from-gray-900 to-black">
       {/* Spinning Icons */}
       <Film
         size={140}
@@ -77,7 +79,7 @@ const AuthForm = () => {
       <div className="w-1/2 h-full pl-32 flex flex-col justify-center">
         <div className="flex items-center gap-3 mb-7">
           <MdMovieFilter size={40} color="#B1D960" />
-          <h3 onClick={() => navigate("/")} className="logofont font-bold text-4xl text-white cursor-pointer">
+          <h3 onClick={() => navigate("/")} className="logofont font-bold text-4xl text-white cursor-pointer mt-2">
             FlickNest
           </h3>
         </div>
@@ -100,81 +102,124 @@ const AuthForm = () => {
       {/* Right Panel - Form */}
       <div className="w-1/2 h-full flex justify-center items-center bg-black/20">
         <div className="w-[500px] bg-transparent">
-          <h3 className="text-center text-3xl font-bold text-white mb-2">
-            {isLogin ? "Welcome Back" : "Join FlickNest"}
-          </h3>
-          <p className="text-center text-gray-400 mb-8">
-            {isLogin ? "Continue your movie journey" : "Start your cinematic adventure"}
-          </p>
+          {!authMethod || authMethod === "none" ? (
+            <>
+              <div className="min-h-screen flex items-center justify-center  px-4">
+                <div className="bg-zinc-900 rounded-2xl shadow-xl max-w-md w-full p-8 sm:p-10">
+                  <h3 className="text-center text-3xl sm:text-4xl font-extrabold text-white mb-10">
+                    Sign in to <span className="text-purple-500">FlickNest</span>
+                  </h3>
 
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 px-5">
-            <div className="space-y-4">
-              {!isLogin && (
-                <>
+                  <button
+                    onClick={() => console.log("Google login logic here")}
+                    className="w-full bg-white text-black py-3 sm:py-4 rounded-xl hover:bg-white/85 flex items-center justify-center gap-3 font-medium text-base sm:text-lg transition duration-200 shadow-md hover:shadow-lg mb-5"
+                  >
+                    <img
+                      src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                      alt="Google Icon"
+                      className="w-5 h-5"
+                    />
+                    Continue with Google
+                  </button>
+
+                  <button
+                    onClick={() => setAuthMethod("email")}
+                    className="w-full border border-white text-white py-3 sm:py-4 rounded-xl hover:bg-white hover:text-black transition duration-200 font-medium text-base sm:text-lg shadow-md hover:shadow-lg"
+                  >
+                    Continue with Email
+                  </button>
+                </div>
+              </div>
+
+            </>
+          ) : (
+            <>
+              <h3 className="text-center text-3xl font-bold text-white mb-2">
+                {isLogin ? "Welcome Back" : "Join FlickNest"}
+              </h3>
+              <p className="text-center text-gray-400 mb-8">
+                {isLogin ? "Continue your movie journey" : "Start your cinematic adventure"}
+              </p>
+
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 px-5">
+                <div className="space-y-4">
+                  {!isLogin && (
+                    <>
+                      <div className={inputDivClass}>
+                        <User size={24} className={inputIconClass} />
+                        <input
+                          type="text"
+                          placeholder="Full Name"
+                          {...register("name")}
+                          className="w-full py-4 outline-none bg-transparent placeholder-gray-400"
+                        />
+                      </div>
+                      {"name" in errors && (  //returns true only if name exists in the error object.
+                        <p className="text-red-400 text-sm px-1">{errors.name?.message}</p>
+                      )}
+                    </>
+                  )}
+
                   <div className={inputDivClass}>
-                    <User size={24} className={inputIconClass} />
+                    <Mail size={24} className={inputIconClass} />
                     <input
                       type="text"
-                      placeholder="Full Name"
-                      {...register("name")}
+                      placeholder="Email Address"
+                      {...register("email")}
                       className="w-full py-4 outline-none bg-transparent placeholder-gray-400"
                     />
                   </div>
-                  {"name" in errors && (  //returns true only if name exists in the error object.
-                    <p className="text-red-400 text-sm px-1">{errors.name?.message}</p>
-                  )}
-                </>
-              )}
+                  {errors.email && <p className="text-red-400 text-sm px-1">{errors.email.message}</p>}
 
-              <div className={inputDivClass}>
-                <Mail size={24} className={inputIconClass} />
-                <input
-                  type="text"
-                  placeholder="Email Address"
-                  {...register("email")}
-                  className="w-full py-4 outline-none bg-transparent placeholder-gray-400"
-                />
-              </div>
-              {errors.email && <p className="text-red-400 text-sm px-1">{errors.email.message}</p>}
+                  <div className={inputDivClass}>
+                    <Lock size={24} className={inputIconClass} />
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      {...register("password")}
+                      className="w-full py-4 outline-none bg-transparent placeholder-gray-400"
+                    />
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(prev => !prev)}
+                      className="text-gray-400 focus:outline-none"
+                    >
+                      {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
+                  </div>
+                  {errors.password && <p className="text-red-400 text-sm px-1">{errors.password.message}</p>}
 
-              <div className={inputDivClass}>
-                <Lock size={24} className={inputIconClass} />
-                <input
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password"
-                  {...register("password")}
-                  className="w-full py-4 outline-none bg-transparent placeholder-gray-400"
-                />
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(prev => !prev)}
-                  className="text-gray-400 focus:outline-none"
-                >
-                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                  <button
+                    type="submit"
+                    className="w-full text-black bg-[#B1D960] py-4 rounded-lg hover:bg-[#a6cc50] flex items-center justify-center gap-3 transition font-bold text-xl logofont"
+                  >
+                    {isLogin ? "Enter FlickNest" : "Join FlickNest"} <Play size={20} />
+                  </button>
+                </div>
+              </form>
+
+              <div className="flex justify-center gap-2 mt-7 text-gray-400 text-sm">
+                <p>{isLogin ? "New to FlickNest?" : "Already have an account?"}</p>
+                <button onClick={() => setIsLogin(prev => !prev)} className="text-[#b1d960] hover:underline cursor-pointer">
+                  {isLogin ? "Create Account" : "Sign In"}
                 </button>
               </div>
-              {errors.password && <p className="text-red-400 text-sm px-1">{errors.password.message}</p>}
 
-              <button
-                type="submit"
-                className="w-full text-black bg-[#B1D960] py-4 rounded-lg hover:bg-[#a6cc50] flex items-center justify-center gap-3 transition font-bold text-xl logofont"
-              >
-                {isLogin ? "Enter FlickNest" : "Join FlickNest"} <Play size={20} />
-              </button>
-            </div>
-          </form>
+              <div className="flex items-center justify-center gap-6 mt-5">
+                {isLogin && (
+                  <button className="text-gray-500 hover:text-gray-400 text-sm hover:underline cursor-pointer">
+                    Forgot your password?
+                  </button>
+                )}
 
-          <div className="flex justify-center gap-2 mt-7 text-gray-400 text-sm">
-            <p>{isLogin ? "New to FlickNest?" : "Already have an account?"}</p>
-            <button onClick={() => setIsLogin(prev => !prev)} className="text-[#b1d960] hover:underline">
-              {isLogin ? "Create Account" : "Sign In"}
-            </button>
-          </div>
-
-          {isLogin && (
-            <button className="w-full mt-5 text-center text-gray-500 hover:text-gray-400 text-sm hover:underline">
-              Forgot your password?
-            </button>
+                <button
+                  onClick={() => setAuthMethod("none")}
+                  className="text-sm text-gray-400 hover:text-white cursor-pointer"
+                >
+                  ← Go back
+                </button>
+              </div>
+            </>
           )}
         </div>
       </div>
