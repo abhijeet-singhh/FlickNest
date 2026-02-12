d2348fb7517ac1bb5c486e636dea643c1b2c53ab
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2026-02-12 09:23:23 +0000

    chore: remove old React + Redux + MongoDB code for clean Next.js migration

--- src/pages/Explore.tsx
+++
@@ -1,7 +0,0 @@
-const Explore = () => {
-  return (
-    <div className="flex justify-center items-center text-white h-screen">Explore</div>
-  )
-}
-
-export default Explore
--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from './Home'
-export { default as Search } from './Search'
-export { default as Explore } from './Explore'
-export { default as Details } from './Details'
