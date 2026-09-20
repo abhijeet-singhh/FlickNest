137a972e683121ec2f8ae1a4b355aff327eca634
Abhijeet Singh <abhijeetsingh0104@gmail.com>
2026-09-20 03:10:57 +0000

    feat: add shared client hooks

---
+++ hooks/use-media-query.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { useCallback, useSyncExternalStore } from "react";
+
+export function useMediaQuery(query: string): boolean {
+  const subscribe = useCallback(
+    (onStoreChange: () => void) => {
+      const mediaQuery = window.matchMedia(query);
+
+      mediaQuery.addEventListener("change", onStoreChange);
+
+      return () => {
+        mediaQuery.removeEventListener("change", onStoreChange);
+      };
+    },
+    [query],
+  );
+
+  const getSnapshot = useCallback(() => {
+    return window.matchMedia(query).matches;
+  }, [query]);
+
+  const getServerSnapshot = useCallback(() => {
+    return false;
+  }, []);
+
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+}
